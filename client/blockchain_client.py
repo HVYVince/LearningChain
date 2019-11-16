1e63d65088adf876b2126e492057c29d35185c94
Maxime Lovino <maximelovino@gmail.com>
2019-11-16 21:09:27 +0100

    We launch and kill containers, showtime

--- client/blockchain_client.py
+++ client/blockchain_client.py
@@ -12,7 +12,7 @@ class BlockchainClient(Thread):
         # TODO here key is id and then we have object with full container and expiration time and image_id
         self.running_containers = {}
         self.started_images = []
-        self.max_exec = 20
+        self.max_exec = 3600
         self.minimal_bounty = 0
         self.minimal_validity = 10  # TODOO to set
         self.account_balance = 0
@@ -38,25 +38,32 @@ class BlockchainClient(Thread):
             self.kill_container(id)
 
     def handle_finished_containers(self):
-        print(self.running_containers)
-        pass
+        running_ids = list(self.running_containers.keys())
+        for id in running_ids:
+            status = docker_client.status(id)
+            if status == 'exited':
+                output = docker_client.retrieve_output(id)
+                print(f"Output was {output}")
+                # TODO here we should then output to blockchain (retrieve model from correct tmp folder)
+                self.kill_container(id)
 
     def find_new_jobs(self):
         # TODO return new jobs from blockchain with filters from parameters
-        return [{"image": "matoran/lauzhack_knn_5@sha256:6520a3942ac2e2c91ab747a22c5d748b7f2e3acbd4414aa5867d752c5d60fb10"}]
+        return [{"image": "matoran/lauzhack_knn_5@sha256:b61e1bcb6353650e44575390812bc237be1c17ec06e974745b1bf380aa4dd94e"}]
 
     def start_new_jobs(self, jobs):
         for job in jobs:
             image_id = job["image"]
             if image_id not in self.started_images:  # TODO perhaps should we check full with the hash as well
-                print(f"Launching new container for image {image_id}")
+                print(
+                    f"Launching new container for image {image_id} for maximum {self.max_exec} seconds")
                 self.started_images.append(image_id)
                 container = docker_client.run_container(image_id)
                 self.running_containers[container.id] = {
                     "container": container, "expiration": time.time() + self.max_exec}
 
     def retrieve_account_balance(self):
-        # TODO retrieve account balance
+        # TODO retrieve account balance when blockchain happens
         self.account_balance = 0
 
     def kill_container(self, id):
@@ -78,5 +85,11 @@ class BlockchainClient(Thread):
             self.max_exec = msg["data"]
         elif msg["type"] == Message.SET_MINIMAL_VALIDITY:
             self.minimal_validity = msg['data']
+        elif msg["type"] == Message.JOB_QUEUE:
+            self.submit_new_job(msg["data"])
         else:
             print("Message not recognized")
+
+    def submit_new_job(self, data):
+        # TODO when blockchain happens
+        print(f"New Job {data}")

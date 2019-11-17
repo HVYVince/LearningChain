5c70ea0a1e8bf4b926c0b5ebea1d33217f5bc790
Maxime Lovino <maximelovino@gmail.com>
2019-11-17 06:55:23 +0100

    changed containers to do validation as well

--- training_containers/simpleKNN.py
+++ training_containers/simpleKNN.py
@@ -10,25 +10,28 @@ training_path = os.getenv("TRAINING_PATH")
 test_path = os.getenv("TEST_PATH")
 
 
-train = np.array(pd.read_csv(training_path, header=None))
-train_X = train[:, :-1]
-train_y = train[:, -1]
+validation = os.getenv("VALIDATION", False)
 
-test = np.array(pd.read_csv(test_path, header=None))
-test_X = train[:, :-1]
-test_y = train[:, -1]
+model_path = os.path.join(os.sep, "tmp", "model")
 
+if not validation:
+    train = np.array(pd.read_csv(training_path, header=None))
+    train_X = train[:, :-1]
+    train_y = train[:, -1]
 
-clf = KNeighborsClassifier(n_neighbors=K_VALUE)
-clf.fit(train_X, train_y)
+    test = np.array(pd.read_csv(test_path, header=None))
+    test_X = train[:, :-1]
+    test_y = train[:, -1]
 
+    clf = KNeighborsClassifier(n_neighbors=K_VALUE)
+    clf.fit(train_X, train_y)
+
+    dump(clf, model_path)
+else:
+    clf = load(model_path)
 
 y_hat = clf.predict(test_X)
 
 accuracy = np.mean(train_y == y_hat)
 
 print(accuracy)
-
-model_path = os.path.join(os.sep, "tmp", "model")
-
-dump(clf, model_path)
--- training_containers/simpleSVM.py
+++ training_containers/simpleSVM.py
@@ -3,32 +3,34 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 import numpy as np
 import pandas as pd
-from joblib import dump
+from joblib import dump, load
 
 
 training_path = os.getenv("TRAINING_PATH")
 test_path = os.getenv("TEST_PATH")
 
+validation = os.getenv("VALIDATION", False)
 
-train = np.array(pd.read_csv(training_path, header=None))
-train_X = train[:, :-1]
-train_y = train[:, -1]
+model_path = os.path.join(os.sep, "tmp", "model")
 
-test = np.array(pd.read_csv(test_path, header=None))
-test_X = train[:, :-1]
-test_y = train[:, -1]
+if not validation:
+    train = np.array(pd.read_csv(training_path, header=None))
+    train_X = train[:, :-1]
+    train_y = train[:, -1]
 
+    test = np.array(pd.read_csv(test_path, header=None))
+    test_X = train[:, :-1]
+    test_y = train[:, -1]
 
-clf = SVC()
-clf.fit(train_X, train_y)
+    clf = SVC()
+    clf.fit(train_X, train_y)
 
+    dump(clf, model_path)
+else:
+    clf = load(model_path)
 
 y_hat = clf.predict(test_X)
 
 accuracy = np.mean(train_y == y_hat)
 
 print(accuracy)
-
-model_path = os.path.join(os.sep, "tmp", "model")
-
-dump(clf, model_path)
